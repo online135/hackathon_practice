4bdf6b5257b911f8bf5694cb0cb8208bb7aa6c8a
DESKTOP-4MPHLE6\User <b97b01067@gmail.com>
2024-08-19 20:28:05 +0800

    temp storage of data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -23,26 +25,52 @@ class TodoListScreen extends StatefulWidget {
 }
 
 class _TodoListScreenState extends State<TodoListScreen> {
-  final List<Map<String, dynamic>> _todoList = [];
+  List<Map<String, dynamic>> _todoList = [];
   final TextEditingController _controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadTodoList();
+  }
+
+  Future<void> _loadTodoList() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? todoListString = prefs.getString('todoList');
+    if (todoListString != null) {
+      setState(() {
+        _todoList = List<Map<String, dynamic>>.from(
+          json.decode(todoListString) as List,
+        );
+      });
+    }
+  }
+
+  Future<void> _saveTodoList() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('todoList', json.encode(_todoList));
+  }
 
   void _addTodoItem(String title) {
     setState(() {
       _todoList.add({'title': title, 'completed': false});
     });
     _controller.clear();
+    _saveTodoList();
   }
 
   void _toggleTodoItem(int index) {
     setState(() {
       _todoList[index]['completed'] = !_todoList[index]['completed'];
     });
+    _saveTodoList();
   }
 
   void _deleteTodoItem(int index) {
     setState(() {
       _todoList.removeAt(index);
     });
+    _saveTodoList();
   }
 
   void _showAddTodoDialog() {
